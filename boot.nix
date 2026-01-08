e90367e7c56fb742728e9f4549814efc269e909a
Prasanna Loganathar <pvl@prasannavl.com>
2026-01-08 13:11:26 +0800

    Configure boot settings

--- boot.nix
+++ boot.nix
@@ -2,7 +2,10 @@
 {
   boot.loader = {
     timeout = 3;
-    systemd-boot.enable = true;
+    systemd-boot = { 
+      enable = true;
+      configurationLimit = 10;
+    };
     efi.canTouchEfiVariables = true;
   };
   boot.initrd.systemd = {
