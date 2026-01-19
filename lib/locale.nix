ca5b94d4ae957a451f9e1146e2e36ae7bb290569
Prasanna Loganathar <pvl@prasannavl.com>
2026-01-19 09:11:49 +0530

    Fix auto timezones

--- lib/locale.nix
+++ lib/locale.nix
@@ -2,9 +2,10 @@
   lib,
   ...
 }: {
-  # We leave it override-able.
-  time.timeZone = lib.mkDefault "Asia/Singapore";
-  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+  # We automatically set this below.
+  # time.timeZone = "Asia/Singapore";
+
+  i18n.defaultLocale = "en_US.UTF-8";
 
   # i18n.extraLocaleSettings = {
   #   LC_ADDRESS = locale;
@@ -18,6 +19,13 @@
   #   LC_TIME = locale;
   # };
 
+  # This doesn't always work however. So 
+  # we use tzupdate as fallback.
   services.automatic-timezoned.enable = true;
   services.geoclue2.geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
+
+  # Automatic timezones based on geo ip.
+  # Not as accurate as automatic-timezoned above
+  # but reliable fallback.
+  services.tzupdate.enable = true;
 }
