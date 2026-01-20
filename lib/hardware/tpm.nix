f3188ca73b74da1102d6f62d6fc14663ac06ced7
Prasanna Loganathar <pvl@prasannavl.com>
2026-01-20 08:46:37 +0530

    Recommended tpm config

--- lib/hardware/tpm.nix
+++ lib/hardware/tpm.nix
@@ -1,10 +1,13 @@
 {...}: {
   boot.initrd.systemd.tpm2.enable = true;
 
+  # Ref: https://github.com/NixOS/nixpkgs/blob/release-25.11/nixos/modules/security/tpm2.md
   security.tpm2 = {
     enable = true;
     abrmd.enable = true;
-    tctiEnvironment.enable = true;
     pkcs11.enable = true;
+
+    tctiEnvironment.enable = true;
+    tctiEnvironment.interface = "tabrmd";
   };
 }
