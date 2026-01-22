68c2cef4175f49a0db4fef53ec7c3d0b4d3fbc4b
Prasanna Loganathar <pvl@prasannavl.com>
2026-01-22 13:26:39 +0530

    Add gtk bookmarks

---
+++ users/pvl/gtk/default.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+let
+  homeDir = config.home.homeDirectory;
+in {
+  gtk = {
+    enable = true;
+    gtk3 = {
+      enable = true;
+      bookmarks = [
+        "file:/// /"
+        "file:///${homeDir}/Documents"
+        "file:///${homeDir}/Downloads"
+        "file:///${homeDir}/Music"
+        "file:///${homeDir}/Pictures"
+        "file:///${homeDir}/Videos"
+        "file:///${homeDir}/.config .config"
+        "file:///${homeDir}/.local .local"
+        "file:///${homeDir}/src src"
+        "file:///${homeDir}/tmp tmp"
+        "file:///${homeDir}/spaces/llmug spaces:llmug"
+        "sftp://pvl-x2/home/pvl ssh:pvl-x2/pvl"
+      ];
+    };
+  };
+}
--- users/pvl/home.nix
+++ users/pvl/home.nix
@@ -13,6 +13,7 @@ in {
     ./git
     ./inputrc
     ./tmux
+    ./gtk
   ];
 
   xdg = {
