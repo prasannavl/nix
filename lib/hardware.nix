f77c03d3b6605bb20bba1732f21fabf242b04ca6
Prasanna Loganathar <pvl@prasannavl.com>
2026-01-25 23:55:46 +0530

    Add more hw core

--- lib/hardware.nix
+++ lib/hardware.nix
@@ -1,11 +1,13 @@
-{...}: {
+{ lib, ...}: {
   # Enable all firmware
   hardware.enableAllFirmware = true;
   hardware.i2c.enable = true;
+  hardware.bluetooth.enable = true;
 
   # Other hardware related services
   services.fwupd.enable = true;
   services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
 
   # For SSDs
   services.fstrim.enable = true;
