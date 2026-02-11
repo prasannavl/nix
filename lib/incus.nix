f9943aec7c52069e94548d5bb2ecd0f51f05c2b9
Prasanna Loganathar <pvl@prasannavl.com>
2026-02-11 16:26:12 +0800

    Add incus preseed

--- lib/incus.nix
+++ lib/incus.nix
@@ -2,5 +2,61 @@
   virtualisation.incus = {
     enable = true;
     ui.enable = true;
+    preseed = {
+      config = {
+        "core.https_address" = "[::]:8443";
+      };
+
+      networks = [
+        {
+          config = {
+            "ipv4.address" = "auto";
+            "ipv6.address" = "auto";
+          };
+          description = "";
+          name = "incusbr0";
+          type = "";
+          project = "default";
+        }
+      ];
+
+      storage_pools = [
+        {
+          config = {
+            source = "/var/lib/incus/storage-pools/default";
+          };
+          description = "";
+          name = "default";
+          driver = "btrfs";
+        }
+      ];
+
+      storage_volumes = [];
+
+      profiles = [
+        {
+          config = {};
+          description = "";
+          devices = {
+            eth0 = {
+              name = "eth0";
+              network = "incusbr0";
+              type = "nic";
+            };
+            root = {
+              path = "/";
+              pool = "default";
+              type = "disk";
+            };
+          };
+          name = "default";
+          project = "default";
+        }
+      ];
+
+      projects = [];
+      certificates = [];
+      cluster = null;
+    };
   };
 }
