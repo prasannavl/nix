f92befaeb0dd5241dfd8a6b48d4c1cb33029575f
Prasanna Loganathar <pvl@prasannavl.com>
2026-02-16 17:21:39 +0800

    Add handbrake

--- hosts/pvl-a1/firewall.nix
+++ hosts/pvl-a1/firewall.nix
@@ -1,7 +1,13 @@
-{...}: {
+{lib, ...}: {
   networking.firewall = {
     trustedInterfaces = ["incusbr0"];
   };
+
+  # networking.firewall.enable = lib.mkForce false;
+
+  # networking.firewall.allowedTCPPortRanges = [
+  #   { from = 32768; to = 60999; }
+  # ];
 
   # networking.nat = {
   #   enable = true;
