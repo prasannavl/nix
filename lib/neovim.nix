a9fb64f11dee4f6942e78a168641120067c3bb3a
Prasanna Loganathar <pvl@prasannavl.com>
2026-02-17 12:36:25 +0800

    Update global nvim

--- lib/neovim.nix
+++ lib/neovim.nix
@@ -4,44 +4,5 @@
     defaultEditor = true;
     vimAlias = true;
     viAlias = true;
-    configure = {
-      customLuaRC = ''
-        vim.g.mapleader = " "
-        require("lazy").setup({
-          spec = {
-            {
-              "LazyVim/LazyVim",
-              dir = "${pkgs.vimPlugins.LazyVim}",
-              import = "lazyvim.plugins",
-            },
-          { import = "lazyvim.plugins.extras.lang.rust" },
-          { import = "plugins.user" },
-          },
-        })
-      '';
-
-      packages.myVimPackage = with pkgs.vimPlugins; {
-        start = [
-          LazyVim
-        ];
-        opt = [ ];
-      };
-
-      runtime."lua/plugins/user.lua".text = ''
-        return {
-          {
-            "LazyVim/LazyVim",
-            opts = {
-              colorscheme = "catppuccin",
-            },
-          }
-        }
-      '';
-    };
   };
-
-  # keep CLI deps here (not plugins)
-  environment.systemPackages = with pkgs; [
-    git ripgrep fd gcc
-  ];
 }
