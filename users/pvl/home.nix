614e37482f4cc5a4f3b1a74f86ecb07cec72f20f
Prasanna Loganathar <pvl@prasannavl.com>
2026-02-17 22:31:31 +0800

    Switch config to dotfiles

--- users/pvl/home.nix
+++ users/pvl/home.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home.preferXdgDirectories = true;
 
   programs.zoxide = {
@@ -19,18 +24,30 @@
     atool
   ];
 
-  home.file = {
-    # ".config/containers/storage.conf".text = ''
-    #   [storage]
-    #   driver = "overlay"
-    #
-    #   [storage.options]
-    #   mount_program = "${pkgs.fuse-overlayfs}/bin/fuse-overlayfs"
-    # '';
-    # ".config/chrome-flags.conf".text = ''
-    #   --disable-smooth-scrolling
-    # '';
-  };
+  home.activation.cloneDotfiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    repo_url="https://github.com/prasannavl/dotfiles.git"
+    dotfiles_dir="${config.home.homeDirectory}/dotfiles"
+
+    if [ -d "$dotfiles_dir/.git" ]; then
+      $DRY_RUN_CMD ${pkgs.git}/bin/git -C "$dotfiles_dir" fetch --all --prune
+      $DRY_RUN_CMD ${pkgs.git}/bin/git -C "$dotfiles_dir" pull --ff-only
+    else
+      $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir -p "${config.home.homeDirectory}"
+      $DRY_RUN_CMD ${pkgs.git}/bin/git clone "$repo_url" "$dotfiles_dir"
+    fi
+  '';
+
+  home.activation.linkEditableBin = lib.hm.dag.entryAfter ["cloneDotfiles"] ''
+    dotfiles_dir="${config.home.homeDirectory}/dotfiles"
+    bin_link="${config.home.homeDirectory}/bin"
+
+    if [ -e "$bin_link" ] && [ ! -L "$bin_link" ]; then
+      echo "Refusing to replace non-symlink path: $bin_link" >&2
+      exit 1
+    fi
+
+    $DRY_RUN_CMD ${pkgs.coreutils}/bin/ln -sfn "$dotfiles_dir/bin" "$bin_link"
+  '';
 
   # The state version is required and should stay at the version you
   # originally installed.
--- users/pvl/neovim/default.nix
+++ users/pvl/neovim/default.nix
@@ -1,26 +1,35 @@
 {
   nixos = {...}: {};
 
-  home = {pkgs, ...}: {
+  home = {config, lib, pkgs, ...}: {
     programs.neovim = {
       enable = true;
       defaultEditor = true;
       viAlias = true;
       vimAlias = true;
-      extraLuaConfig = builtins.readFile ./init.lua;
-      plugins = [
+      # We don't do config here, we link the whole nvim config from our dotfiles.
+      # plugins = [
         # We basically use the LazyVim distro instead
         # of using the lazy plugin manager directly.
         # We let it download and manage. 
         # Note: mason and friends, will not work without nixld.
         # pkgs.vimPlugins.lazy-nvim
-      ];
+      # ];
     };
 
-    xdg.configFile."nvim/lua" = {
-      source = ./lua;
-      recursive = true;
-    };
+    home.activation.linkEditableNvim = lib.hm.dag.entryAfter ["cloneDotfiles"] ''
+      dotfiles_nvim="${config.home.homeDirectory}/dotfiles/nvim/.config/nvim"
+      nvim_link="${config.home.homeDirectory}/.config/nvim"
+
+      $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir -p "${config.home.homeDirectory}/.config"
+
+      if [ -e "$nvim_link" ] && [ ! -L "$nvim_link" ]; then
+        echo "Refusing to replace non-symlink path: $nvim_link" >&2
+        exit 1
+      fi
+
+      $DRY_RUN_CMD ${pkgs.coreutils}/bin/ln -sfn "$dotfiles_nvim" "$nvim_link"
+    '';
 
     home.packages = with pkgs; [
       git
