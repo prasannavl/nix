d66eb21d95c85fc2b707c93eddd4d246cdc045cd
Prasanna Loganathar <pvl@prasannavl.com>
2026-03-13 09:28:51 +0800

    Isolate openssh

--- lib/profiles/systemd-container.nix
+++ lib/profiles/systemd-container.nix
@@ -7,6 +7,7 @@
   imports = [
     (modulesPath + "/virtualisation/lxc-container.nix")
     (modulesPath + "/virtualisation/lxc-image-metadata.nix")
+    ../openssh.nix
     ../options.nix
     ../nix.nix
     ../systemd.nix
@@ -59,7 +60,6 @@
     enable = true;
   };
 
-  services.openssh.enable = true;
   services.tailscale.enable = true;
   services.fail2ban.enable = true;
 
