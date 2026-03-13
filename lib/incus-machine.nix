12cbe383e04016ccb600925e7807f06ac561e1c0
Prasanna Loganathar <pvl@prasannavl.com>
2026-03-13 09:40:33 +0800

    Tighen tailscale block

--- lib/incus-machine.nix
+++ lib/incus-machine.nix
@@ -1,51 +1,52 @@
 {
   hostName,
-  tailscaleAuthKeyName ? hostName,
+  tailscaleKey ? hostName,
 }: {
   config,
   lib,
   ...
-}: let
-  tailscaleAuthKeyAgeSource =
-    if tailscaleAuthKeyName == null || tailscaleAuthKeyName == ""
-    then null
-    else ../data/secrets + "/tailscale/${tailscaleAuthKeyName}.key.age";
-  tailscaleAuthKeyAge =
-    if tailscaleAuthKeyAgeSource == null || !builtins.pathExists tailscaleAuthKeyAgeSource
-    then null
-    else builtins.path {
-      path = tailscaleAuthKeyAgeSource;
-      name = "tailscale-${tailscaleAuthKeyName}-auth-key.age";
-    };
-in
-  {
-    systemd.tmpfiles.rules = [
-      "d /var/lib/machine 0700 root root -"
-    ];
+}:
+{
+  systemd.tmpfiles.rules = [
+    "d /var/lib/machine 0700 root root -"
+  ];
 
-    services.openssh.hostKeys = [
-      {
-        path = "/var/lib/machine/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-      {
-        path = "/var/lib/machine/ssh_host_rsa_key";
-        type = "rsa";
-        bits = 4096;
-      }
-    ];
-  }
-  // lib.mkIf (tailscaleAuthKeyAge != null) {
-    age.secrets.tailscale-auth-key = {
-      file = tailscaleAuthKeyAge;
-    };
+  services.openssh.hostKeys = [
+    {
+      path = "/var/lib/machine/ssh_host_ed25519_key";
+      type = "ed25519";
+    }
+    {
+      path = "/var/lib/machine/ssh_host_rsa_key";
+      type = "rsa";
+      bits = 4096;
+    }
+  ];
+}
+// (
+  let
+    k =
+      if tailscaleKey == null || tailscaleKey == ""
+      then null
+      else ../data/secrets + "/tailscale/${tailscaleKey}.key.age";
+  in
+    lib.mkIf (k != null && builtins.pathExists k) {
+      age.secrets.tailscale-auth-key = {
+        file = builtins.path {
+          path = k;
+          name = "tailscale-${tailscaleKey}-auth-key.age";
+        };
+      };
 
-    services.tailscale.authKeyFile = config.age.secrets.tailscale-auth-key.path;
-    services.tailscale.authKeyParameters = {
-      ephemeral = false;
-      preauthorized = true;
-    };
-    services.tailscale.extraUpFlags = [
-      "--advertise-tags=tag:vm"
-    ];
-  }
+      services.tailscale = {
+        authKeyFile = config.age.secrets.tailscale-auth-key.path;
+        authKeyParameters = {
+          ephemeral = false;
+          preauthorized = true;
+        };
+        extraUpFlags = [
+          "--advertise-tags=tag:vm"
+        ];
+      };
+    }
+)
