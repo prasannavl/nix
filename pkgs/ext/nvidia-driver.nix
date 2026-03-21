9e50041ceb7ad0ceeb605c0010773fd93088adb2
Prasanna Loganathar <pvl@prasannavl.com>
2026-03-21 21:28:52 +0800

    Update nvidia driver

--- pkgs/ext/nvidia-driver.nix
+++ pkgs/ext/nvidia-driver.nix
@@ -1,8 +1,8 @@
 {kernelPackages}:
 kernelPackages.nvidiaPackages.mkDriver {
-  version = "580.126.18";
-  sha256_64bit = "sha256-p3gbLhwtZcZYCRTHbnntRU0ClF34RxHAMwcKCSqatJ0=";
-  openSha256 = "sha256-1Q2wuDdZ6KiA/2L3IDN4WXF8t63V/4+JfrFeADI1Cjg=";
-  settingsSha256 = "sha256-QMx4rUPEGp/8Mc+Bd8UmIet/Qr0GY8bnT/oDN8GAoEI=";
-  persistencedSha256 = "sha256-ZBfPZyQKW9SkVdJ5cy0cxGap2oc7kyYRDOeM0XyfHfI=";
+  version = "580.142";
+  sha256_64bit = "sha256-IJFfzz/+icNVDPk7YKBKKFRTFQ2S4kaOGRGkNiBEdWM=";
+  openSha256 = "sha256-v968LbRqy8jB9+yHy9ceP2TDdgyqfDQ6P41NsCoM2AY=";
+  settingsSha256 = "sha256-BnrIlj5AvXTfqg/qcBt2OS9bTDDZd3uhf5jqOtTMTQM=";
+  persistencedSha256 = "sha256-il403KPFAnDbB+dITnBGljhpsUPjZwmLjGt8iPKuBqw=";
 }
