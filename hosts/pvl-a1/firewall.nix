ab048693fa4291729bd4cc80659b87aa80023848
Prasanna Loganathar <pvl@prasannavl.com>
2026-04-20 12:10:48 +0800

    Allow WiFi-Direct ports

--- hosts/pvl-a1/firewall.nix
+++ hosts/pvl-a1/firewall.nix
@@ -3,11 +3,15 @@
     trustedInterfaces = ["incusbr0"];
   };
 
+  # Common debug config
   # networking.firewall.enable = lib.mkForce false;
-
+  # networking.firewall.allowedUDPPortRanges = [ { from = 1024; to = 65535; } ];
   # networking.firewall.allowedTCPPortRanges = [
   #   { from = 32768; to = 60999; }
   # ];
+
+  networking.firewall.allowedTCPPorts = [ 7236 7250 ];
+  networking.firewall.allowedUDPPorts = [ 7236 5353 ];
 
   # networking.nat = {
   #   enable = true;
