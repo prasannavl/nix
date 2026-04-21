a2371cdf9243d960a60169ffdb70f27175682c14
Prasanna Loganathar <pvl@prasannavl.com>
2026-04-21 14:31:38 +0800

    Fix kanshi ordering

--- users/pvl/kanshi/profiles.nix
+++ users/pvl/kanshi/profiles.nix
@@ -4,14 +4,16 @@
       output "${outputs.a1.name}" enable position 0,0
     }
 
-    profile default-extra {
-      output "${outputs.a1.name}" enable position 0,0
-      output "*" enable
-    }
-
     profile home-sg-lguw {
       output "${outputs.a1.name}" enable position 0,320
       output "${outputs.lg-uw3840.name}" enable position 2048,0
+    }
+
+    # Keep wildcard extras last so they do not outrank precise matches.
+
+    profile default-extra {
+      output "${outputs.a1.name}" enable position 0,0
+      output "*" enable
     }
 
     profile home-sg-lguw-extra {
