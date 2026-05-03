920b9f53830a35662f4121f61586a257bff62571
Prasanna Loganathar <pvl@prasannavl.com>
2026-05-04 00:57:29 +0800

    Add rust isolated example

---
+++ pkgs/examples/hello-rust-isolated/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "isolated hello-rust sample app";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    defaultSystem = builtins.head flake-utils.lib.defaultSystems;
+    defaultPkgs = nixpkgs.legacyPackages.${defaultSystem};
+    pkgHelper = import ../../../lib/flake/pkg-helper.nix;
+    mkOutputs = pkgs: let
+      drv = pkgs.callPackage ./default.nix {};
+    in {
+      flakeOutputs = pkgHelper.mkStdFlakeOutputs {
+        pkgs = pkgs;
+        build = drv;
+      };
+      nixosModules = pkgHelper.mkNixosModuleAttrs {
+        build = drv;
+      };
+    };
+    defaultOutputs = mkOutputs defaultPkgs;
+  in
+    flake-utils.lib.eachDefaultSystem (system:
+      (mkOutputs nixpkgs.legacyPackages.${system}).flakeOutputs)
+    // {
+      nixosModules = defaultOutputs.nixosModules;
+    };
+}
