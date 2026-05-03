b63537de4a6f8e79128699514be51b28ce22c4ab
Prasanna Loganathar <pvl@prasannavl.com>
2026-05-04 00:43:13 +0800

    Eliminate remaining nix path usage

--- pkgs/cloudflare-apps/flake.nix
+++ pkgs/cloudflare-apps/flake.nix
@@ -4,25 +4,17 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
-    nixbot = {
-      url = "path:../nixbot";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
-    nixbot,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       pkgHelper = import ../../lib/flake/pkg-helper.nix;
-      drv = pkgs.callPackage ./default.nix {
-        nixbot = nixbot.packages.${system}.default;
-      };
+      drv = pkgs.callPackage ./default.nix {};
     in
       pkgHelper.mkStdFlakeOutputs {
         inherit pkgs;
