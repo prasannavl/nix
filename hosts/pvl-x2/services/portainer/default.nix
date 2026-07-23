841d77b6aa273bb6963186655cfa8990a4f9a3ec
Prasanna Loganathar <pvl@prasannavl.com>
2026-07-23 09:40:47 +0800

    fix(portainer): probe HTTPS endpoint

--- hosts/pvl-x2/services/portainer/default.nix
+++ hosts/pvl-x2/services/portainer/default.nix
@@ -1,11 +1,14 @@
 {...}: {
   config.services.podman-compose.pvl.instances.portainer = {podmanSocket, ...}: rec {
     exposedPorts = {
-      http = {
+      edge = {
         port = 8001;
         openFirewall = true;
       };
-      https.port = 9444;
+      http = {
+        port = 9444;
+        upstreamProtocol = "https";
+      };
     };
 
     source = ''
@@ -16,8 +19,8 @@
           image: docker.io/portainer/portainer-ce:2.43.0
           container_name: portainer
           ports:
-            - "${toString exposedPorts.http.port}:8000"
-            - "${toString exposedPorts.https.port}:9443"
+            - "${toString exposedPorts.edge.port}:8000"
+            - "${toString exposedPorts.http.port}:9443"
           privileged: true
           volumes:
             - type: bind
