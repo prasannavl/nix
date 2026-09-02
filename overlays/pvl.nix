b67d026003de9a20e710766e39da0669bc75efb0
Prasanna Loganathar <pvl@prasannavl.com>
2026-09-02 16:32:19 +0800

    chore(p7-cmds): update ver

--- overlays/pvl.nix
+++ overlays/pvl.nix
@@ -1,9 +1,9 @@
 {inputs}: final: prev: let
   inherit (final.stdenv.hostPlatform) system;
   inherit (inputs.p7-borders.packages.${system}) p7-borders;
-  # inherit (inputs.p7-cmds.packages.${system}) p7-cmds;
+  inherit (inputs.p7-cmds.packages.${system}) p7-cmds;
   # p7-borders = final.callPackage ../lib/ext/gnome-ext/p7-borders.nix {};
-  p7-cmds = final.callPackage ../lib/ext/gnome-ext/p7-cmds.nix {};
+  # p7-cmds = final.callPackage ../lib/ext/gnome-ext/p7-cmds.nix {};
   vimPlugins = final.callPackage ../lib/ext/neovim-plugins {};
 in rec {
   pvl = {
