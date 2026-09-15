00f6e26905817a401eaa2d0e4676be6216821161
Prasanna Loganathar <pvl@prasannavl.com>
2026-09-16 00:29:29 +0800

    chore(gnome): update P7 Borders to 53

--- lib/ext/gnome-ext/sources.nix
+++ lib/ext/gnome-ext/sources.nix
@@ -2,8 +2,8 @@
   p7-borders = {
     kind = "gnome-extension";
     uuid = "p7-borders@prasannavl.com";
-    version = "52";
-    hash = "sha256-8J7cHc9W2UH44907hNy4z+mWMDdl1XAlVCFFaSIhPTc=";
+    version = "53";
+    hash = "sha256-ttV1+vUBbIWBIcgIS+LaRmxA4KKkpHMZrBiXUl7CGEQ=";
   };
 
   p7-cmds = {
