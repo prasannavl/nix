c0a90749158681a683000b3e875d2f55f53b6922
Prasanna Loganathar <pvl@prasannavl.com>
2026-09-20 23:42:06 +0800

    chore(vscode): update to 1.138.0

--- lib/ext/vscode/sources.nix
+++ lib/ext/vscode/sources.nix
@@ -1,22 +1,22 @@
 {
   vscode = {
     kind = "vscode-stable";
-    version = "1.137.0";
-    rev = "645f29cc3176500b4b5762ba887cf2a7f0ffdf2c";
+    version = "1.138.0";
+    rev = "7debcd0e2acdea1c52de81bf9ee1620444407dda";
     srcNames = {
-      x86_64-linux = "code-stable-x64-1788901890.tar.gz";
-      aarch64-linux = "code-stable-arm64-1788901921.tar.gz";
+      x86_64-linux = "code-stable-x64-1789458585.tar.gz";
+      aarch64-linux = "code-stable-arm64-1789458511.tar.gz";
       aarch64-darwin = "VSCode-darwin-arm64.zip";
     };
     srcHashes = {
-      x86_64-linux = "sha256-iHHWNyklaONMLSfapTIKOzyAX0H5QBTfzq++YTQYf84=";
-      aarch64-linux = "sha256-5q4qiAePrceAEcS92nDFlmC1/iQrSqs0EoVWB3nFIxM=";
-      aarch64-darwin = "sha256-Fu5c3bHqGSNOHyUW2gfVfgfXyrarRaVRXasHdlbLxl4=";
+      x86_64-linux = "sha256-WfsPiOqy/i46BTrAIOGPyytcET0lXkHILSYwK2ddWAw=";
+      aarch64-linux = "sha256-vwGtsZGav1pVa0nl1ShEBEDMmodft7S/el78q54ah7g=";
+      aarch64-darwin = "sha256-zLcZMK+TIUyu5ERU5OQfQXSFdzMYaZd/Ahmy/V5OYBY=";
     };
     serverHashes = {
-      x86_64-linux = "sha256-bf5KW7f0tQnALS6KbyWMzZE7kQaLVT0DmtaPKazmITY=";
-      aarch64-linux = "sha256-/ArmjIc9DlKs6Tnl96yxzYKBvhyWD3i49ez97vMeZ/w=";
-      aarch64-darwin = "sha256-CKvY9cJ6CCAMXwR+Q1yNT8vk6LZXMQaoDtIfbeh0lH4=";
+      x86_64-linux = "sha256-92ZHZZLNn4denn5m5IPbp+lmG3lNjbylZiSRJtNSUyQ=";
+      aarch64-linux = "sha256-ou12JUSS3jJAu96dW+IA6jtcEUPhDgYiCK645nQV/Ro=";
+      aarch64-darwin = "sha256-Mrfooj7ECXe5XcppKMPD/YVKayFo4VDloRhdTFiuPpQ=";
     };
   };
 }
