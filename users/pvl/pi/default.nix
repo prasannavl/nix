f7b3f6d5e741a125b582778416d91fdb984808bb
Prasanna Loganathar <pvl@prasannavl.com>
2026-09-26 15:22:08 +0800

    feat(pvl): install revdiff and pi-diff

--- users/pvl/pi/default.nix
+++ users/pvl/pi/default.nix
@@ -9,10 +9,15 @@
     piSubagents = pkgs.callPackage ../../../lib/ext/pi/pi-subagents {};
     piTps = pkgs.callPackage ../../../lib/ext/pi/pi-tps {};
     piWeb = pkgs.callPackage ../../../lib/ext/pi/pi-web {};
+    piDiff = pkgs.callPackage ../../../lib/ext/pi/pi-diff {};
+    revdiff = pkgs.callPackage ../../../lib/ext/revdiff {};
+    revdiffPi = pkgs.callPackage ../../../lib/ext/revdiff/pi.nix {};
     piModelsDiscoveryRoot = "${piModelsDiscovery}/share/pi/packages/pi-models-discovery";
     piSessionManagerRoot = "${piSessionManager}/share/pi/packages/pi-session-manager";
     piSubagentsRoot = "${piSubagents}/lib/node_modules/pi-subagents";
     piTpsRoot = "${piTps}/share/pi/packages/pi-tps";
+    piDiffRoot = "${piDiff}/lib/node_modules/@heyhuynhgiabuu/pi-diff";
+    revdiffPiRoot = "${revdiffPi}/share/pi/packages/revdiff-pi";
     subagentPrompts = [
       "council"
       "gather-context-and-clarify"
@@ -22,16 +27,18 @@
       "review-loop"
     ];
   in {
-    home.packages = [pkgs.unstable.pi-coding-agent piWeb];
+    home.packages = [pkgs.unstable.pi-coding-agent piWeb revdiff];
 
     home.file =
       {
+        ".pi/agent/extensions/pi-diff".source = piDiffRoot;
         ".pi/agent/extensions/pi-extensions-i18n".source = "${piModelsDiscoveryRoot}/node_modules/pi-extensions-i18n";
         ".pi/agent/extensions/pi-models-discovery/index.ts".source = "${piModelsDiscoveryRoot}/index.ts";
         ".pi/agent/extensions/pi-models-discovery/locales".source = "${piModelsDiscoveryRoot}/locales";
         ".pi/agent/extensions/pi-models-discovery/node_modules".source = "${piModelsDiscoveryRoot}/node_modules";
         ".pi/agent/extensions/pi-models-discovery/package.json".source = "${piModelsDiscoveryRoot}/package.json";
         ".pi/agent/extensions/pi-models-discovery/src".source = "${piModelsDiscoveryRoot}/src";
+        ".pi/agent/extensions/pi-revdiff".source = revdiffPiRoot;
         ".pi/agent/extensions/pi-session-manager.ts".source = "${piSessionManagerRoot}/extensions/session-manager.ts";
         ".pi/agent/extensions/pi-subagents".source = piSubagentsRoot;
         ".pi/agent/extensions/pi-tps.ts".source = "${piTpsRoot}/extensions/pi-tps.ts";
@@ -39,6 +46,7 @@
         ".pi/agent/skills/pi-models-discovery/SKILL.md".source = "${piModelsDiscoveryRoot}/SKILL.md";
         ".pi/agent/skills/council-mode".source = "${piSubagentsRoot}/skills/council-mode";
         ".pi/agent/skills/pi-subagents".source = "${piSubagentsRoot}/skills/pi-subagents";
+        ".pi/agent/skills/revdiff".source = "${revdiffPiRoot}/plugins/pi/skills/revdiff";
       }
       // builtins.listToAttrs (map (name: {
           name = ".pi/agent/prompts/${name}.md";
